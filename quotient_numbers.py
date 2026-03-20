6074e95bebb3938fb04357cfc5942950ed6c5d22
1acjanepaas <118149690+1acjanepaas@users.noreply.github.com>
2026-03-20 19:41:40 +0800

    Update quotient_numbers.py

--- quotient_numbers.py
+++ quotient_numbers.py
@@ -1,6 +1,6 @@
 #Printing 2 numbers
-first_number = float(input("Enter the first number: "))
-second_number = float(input("Enter the second number: "))
+first_number = int(input("Enter the first number: "))
+second_number = int(input("Enter the second number: "))
 
 #Put remainder
 remainder = first_number % second_number
